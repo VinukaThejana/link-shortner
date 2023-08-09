412476838d2b7d2d5b86494fff2a9c36ffefe9b7
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-08-09 23:57:19 +0530

    Added the user model

---
+++ backend/models/user.model.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+// User is a struct representing the user table in the datbase which contians information
+// about the reigstred users that are in the platform
+type User struct {
+	ID         uint64     `gorm:"primaryKey;autoIncrement:true"`
+	Name       string     `gorm:"type:varchar(100);not null"`
+	Username   string     `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Email      string     `gorm:"type:varchar(100);uniqueIndex"`
+	Role       *string    `gorm:"type:varchar(50);default:'user';not null"`
+	Provider   *string    `gorm:"type:varchar(50);default:'local';not null"`
+	ProviderID string     `gorm:"type:varchar(100)"`
+	Verified   *bool      `gorm:"not null;default:false"`
+	CreatedAt  *time.Time `gorm:"not null;default:now()"`
+	UpdatedAt  *time.Time `gorm:"not null;default:now()"`
+	Sessions   []Sessions `gorm:"foreignKey:UserID"`
+}
