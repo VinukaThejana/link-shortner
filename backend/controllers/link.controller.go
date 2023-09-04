20558a129839e41e5035a40c01bcdadaabfb63e8
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-05 01:21:26 +0530

    Added the ability to create a new link

--- backend/controllers/link.controller.go
+++ backend/controllers/link.controller.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"github.com/VinukaThejana/link-shortner/backend/errors"
 	"github.com/VinukaThejana/link-shortner/backend/initializers"
+	"github.com/VinukaThejana/link-shortner/backend/services"
 	"github.com/VinukaThejana/link-shortner/backend/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/lithammer/shortuuid/v4"
 )
 
 // Links contains all the operations that are related to links
@@ -30,7 +32,48 @@ func (Links) New(c *fiber.Ctx, h *initializers.H) error {
 		})
 	}
 
-	_, err := utils.Session{}.Get(c)
+	linkS := services.Link{H: h}
+	key := ""
+
+	if payload.Key == "" {
+		for i := 1; i <= 4; i++ {
+			newKey := shortuuid.New()
+			ok, err := linkS.IsKeyAvailable(newKey)
+			if err != nil {
+				log.Error(err, nil)
+				break
+			}
+
+			if ok {
+				key = newKey
+				break
+			}
+		}
+
+		if key == "" {
+			return c.Status(fiber.StatusInternalServerError).JSON(response{
+				Status: errors.ErrInternalServerError.Error(),
+			})
+		}
+	} else {
+		ok, err := linkS.IsKeyAvailable(payload.Key)
+		if err != nil {
+			log.Error(err, nil)
+			return c.Status(fiber.StatusInternalServerError).JSON(response{
+				Status: errors.ErrInternalServerError.Error(),
+			})
+		}
+
+		if !ok {
+			return c.Status(fiber.StatusBadRequest).JSON(response{
+				Status: errors.ErrKeyAlreadyUsed.Error(),
+			})
+		}
+
+		key = payload.Key
+	}
+
+	userD, err := utils.Session{}.Get(c)
 	if err != nil {
 		log.Error(err, nil)
 		return c.Status(fiber.StatusInternalServerError).JSON(response{
@@ -38,7 +81,17 @@ func (Links) New(c *fiber.Ctx, h *initializers.H) error {
 		})
 	}
 
-	return c.Status(fiber.StatusOK).JSON(response{
-		Status: errors.Okay,
+	err = linkS.Create(userD, payload.Link, key)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status": errors.Okay,
+		"key":    key,
+		"url":    payload.Link,
 	})
 }
