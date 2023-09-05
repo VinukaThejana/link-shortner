73e5dfe06b64b7a72c267254f7338b480e63ca83
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-05 09:14:39 +0530

    Added the controller function for link related activites
    Added the ability to create, get all, delete and delete all links under a given user

--- backend/controllers/link.controller.go
+++ backend/controllers/link.controller.go
@@ -7,6 +7,7 @@ import (
 	"github.com/VinukaThejana/link-shortner/backend/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/lithammer/shortuuid/v4"
+	"gorm.io/gorm"
 )
 
 // Links contains all the operations that are related to links
@@ -93,5 +94,95 @@ func (Links) New(c *fiber.Ctx, h *initializers.H) error {
 		"status": errors.Okay,
 		"key":    key,
 		"url":    payload.Link,
+	})
+}
+
+// GetLinks is a function to get all the links created by the user
+func (Links) GetLinks(c *fiber.Ctx, h *initializers.H) error {
+	userD, err := utils.Session{}.Get(c)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	linkS := services.Link{H: h}
+
+	links, err := linkS.GetLinks(userD)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"links": links,
+	})
+}
+
+// DeleteLink is a function to delete a link with a given key
+func (Links) DeleteLink(c *fiber.Ctx, h *initializers.H) error {
+	var payload struct {
+		Key string `json:"key" validate:"required,min=3"`
+	}
+
+	if err := c.BodyParser(&payload); err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusBadRequest).JSON(response{
+			Status: errors.ErrBadRequest.Error(),
+		})
+	}
+
+	userD, err := utils.Session{}.Get(c)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	linkS := services.Link{H: h}
+	err = linkS.DeleteLink(userD, payload.Key)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusBadRequest).JSON(response{
+				Status: errors.ErrKeyDoesNotExsist.Error(),
+			})
+		}
+
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response{
+		Status: errors.Okay,
+	})
+}
+
+// DeleteLinks is a function that is used to delete all the links that belong to the user
+func (Links) DeleteLinks(c *fiber.Ctx, h *initializers.H) error {
+	userD, err := utils.Session{}.Get(c)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	linkS := services.Link{H: h}
+	err = linkS.DeleteLinks(userD)
+	if err != nil {
+		log.Error(err, nil)
+		return c.Status(fiber.StatusInternalServerError).JSON(response{
+			Status: errors.ErrInternalServerError.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response{
+		Status: errors.Okay,
 	})
 }
