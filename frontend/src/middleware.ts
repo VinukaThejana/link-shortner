d4b722608ae4f3b1fab045c37459454ad61b2203
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-06 09:11:22 +0530

    Added the ability to detect the user login state from the middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,5 +1,17 @@
 import { defineMiddleware } from "astro/middleware"
+import { getUser, isSessionValid } from "./utils/token";
 
-export const onRequest = defineMiddleware((context, next) => {
+export const onRequest = defineMiddleware(async ({ cookies, locals, redirect }, next) => {
+  const session = cookies.get("session")
+  if (!session || !session.value || !(await isSessionValid(session.value))) {
+    return redirect('/something')
+  }
+
+  const user = getUser(session.value)
+  if (!user) {
+    return redirect('/something')
+  }
+
+  locals.user = user
   return next();
 })
