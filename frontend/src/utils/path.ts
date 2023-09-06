3664497aa117ad7df5b48eb89064e5e10b8d3cdc
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-06 13:51:09 +0530

    Added the ability to get the backend path

---
+++ frontend/src/utils/path.ts
@@ -0,0 +1,19 @@
+
+export const getBackendPath = (path: string, params?: string[]): string => {
+  if (!Array.isArray(params)) {
+    return `${import.meta.env.BACKEND}${path}`
+  }
+
+  var p = "";
+  var i = 0;
+  for (const param of params) {
+    if (i === params.length - 1) {
+      p += param;
+      continue;
+    }
+
+    p += `${param}&`
+  }
+
+  return `${import.meta.env.BACKEND}${path}?${p}`
+}
