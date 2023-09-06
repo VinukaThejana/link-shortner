069d4540b3dc2b16d641e26aa548568d31145d16
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-06 14:27:00 +0530

    Added the proper ability to logout

--- frontend/src/components/navbar/Profile.tsx
+++ frontend/src/components/navbar/Profile.tsx
@@ -1,6 +1,7 @@
 import type { User } from "../../types/session";
+import { getBackendPath } from "../../utils/path";
 
-export const Profile = ({ user }: { user: User | null }) => {
+export const Profile = ({ user }: { user: User | null; }) => {
   return (
     <>
       {user ? (
@@ -16,17 +17,16 @@ export const Profile = ({ user }: { user: User | null }) => {
             className="p-2 mt-3 w-52 border shadow menu menu-sm dropdown-content z-[1] bg-base-100 rounded-box border-slate-800"
           >
             <li>
-              <button
-              >
+              <a href={getBackendPath("/auth/logout", [`state=${encodeURI(window.location.href)}`])}>
                 Logout
-              </button>
+              </a>
             </li>
           </ul>
         </div>
       </>
       ) : (
         <>
-          <a className="btn btn-ghost" href="http://localhost:8080/auth/login">
+          <a className="btn btn-ghost" href={getBackendPath("/auth/login", [`state=${encodeURI(window.location.href)}`])}>
             Login
           </a>
         </>
