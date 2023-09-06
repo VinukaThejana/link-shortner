8b507a805c14d6330c5ac32ae57954caeba01171
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-06 09:07:45 +0530

    Added the user to astro locals

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+declare namespace App {
+  interface Locals {
+    user: {
+      email: string
+      name: string
+      photo_url: string
+      username: string
+    }
+  }
+}
