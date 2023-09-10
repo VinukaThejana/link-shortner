e8ee9950afcb40c99e357b6efb557e7e25933ee8
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-10 20:31:19 +0530

    Added the ability to delete links created by the user

--- frontend/src/components/links/Get.tsx
+++ frontend/src/components/links/Get.tsx
@@ -1,5 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/react-query"
-import { getLinks } from "../../utils/queryFn"
+import { LIMIT, deleteLink, getLinks } from "../../utils/queryFn"
+import { string } from "zod"
+import { toast } from "react-hot-toast"
 
 export const Get = () => {
   const {
@@ -8,7 +10,8 @@ export const Get = () => {
     fetchNextPage,
     isFetchingNextPage,
     isFetchingPreviousPage,
-    isFetching
+    isFetching,
+    refetch
   } = useInfiniteQuery<{
     data: Link[],
     nextPage: number | null | undefined
@@ -19,6 +22,19 @@ export const Get = () => {
     keepPreviousData: true
   })
 
+  const deleteLinks = async (key: string, count: number) => {
+    const state = await deleteLink(key)
+    if (state === "fail") {
+      toast.error("Deletion failed")
+      return
+    }
+
+
+    toast.success("Deleted the link successfully")
+    const page = Math.floor(count/LIMIT)
+    await refetch()
+  }
+
   return (
     <div className="flex flex-col gap-3 mt-24">
       <>
@@ -28,8 +44,8 @@ export const Get = () => {
               <div className="flex flex-col gap-10">
                 {data.pages.map((links) => (
                   <>
-                    {links.data.map((link) => (
-                      <span className="flex flex-col gap-4 sm:flex-row sm:gap-4">
+                    {links.data.map((link, index) => (
+                      <span className="flex flex-col gap-4 sm:flex-row sm:gap-4" key={index}>
                         <label
                           className="w-80 text-black sm:w-72 input bg-[#D7D5D5] truncate inline-flex items-center justify-self-center"
                         >
@@ -51,6 +67,7 @@ export const Get = () => {
                         </span>
                         <span
                           className="hidden w-44 text-red-400 cursor-pointer sm:inline-flex sm:w-8"
+                          onClick={async () => await deleteLinks(link.Key, index)}
                         >
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
