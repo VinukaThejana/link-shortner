4601e7f83c775745368e4077aa9170f874a12035
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-10 20:16:06 +0530

    Added the ability to delete a link that belongs to a user

--- frontend/src/utils/queryFn.ts
+++ frontend/src/utils/queryFn.ts
@@ -1,11 +1,12 @@
 import { getBackendPath } from "./path"
 
+export const LIMIT = 5;
+
 export const getLinks = async (page: number): Promise<{
   data: Link[],
   nextPage: number | null | undefined
 }> => {
-  const limit = 2
-  const res = await fetch(getBackendPath("/links", [`page=${page}`, `limit=${limit}`]), {
+  const res = await fetch(getBackendPath("/links", [`page=${page}`, `limit=${LIMIT}`]), {
     headers: {
       "Content-Type": "application/json"
     },
@@ -29,4 +30,23 @@ export const getLinks = async (page: number): Promise<{
     data: links,
     nextPage
   }
+}
+
+export const deleteLink = async (key: string): Promise<"success" | "fail"> => {
+  const res = await fetch(getBackendPath("/links/delete"), {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({
+      "key": key
+    })
+  })
+
+  if (res.status !== 200) {
+    return "fail"
+  }
+
+  return "success"
 }
