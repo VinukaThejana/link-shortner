b57096825b0af93802ee17a3a1e6f53f5ff68a0f
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-13 14:30:17 +0530

    Added a function to get the details of the aceess token from the client side

--- frontend/src/utils/token.ts
+++ frontend/src/utils/token.ts
@@ -1,44 +1,73 @@
 import { User } from "@/types/session";
 import * as jose from "jose";
+import { getCookie } from "./utils";
 
 export async function verifySession(token: string): Promise<boolean> {
   try {
-    const alg = "HS256"
-    const key = new TextEncoder().encode(process.env.SESSION_TOKEN_SECRET)
+    const alg = "HS256";
+    const key = new TextEncoder().encode(process.env.SESSION_TOKEN_SECRET);
 
     try {
       const { payload } = await jose.jwtVerify(token, key, {
-        algorithms: [alg]
-      })
+        algorithms: [alg],
+      });
       if (!payload.exp) {
-        return false
+        return false;
       }
 
-      const now = Math.floor(new Date().getTime()/1000)
+      const now = Math.floor(new Date().getTime() / 1000);
       if (now >= payload.exp) {
-        return false
+        return false;
       }
 
-      return true
+      return true;
     } catch (error) {
-      return false
+      return false;
     }
   } catch (error) {
-    console.error(error)
-    return false
+    console.error(error);
+    return false;
   }
 }
 
 export async function getUser(token: string): Promise<User | null> {
   try {
     if (!(await verifySession(token))) {
-      return null
+      return null;
     }
 
-    const data = jose.decodeJwt(token) as User
-    return data
+    const data = jose.decodeJwt(token) as User;
+    return data;
   } catch (error) {
-    console.error(error)
-    return null
+    console.error(error);
+    return null;
   }
 }
+
+type AccessToken = {
+  iat: number;
+  exp: number;
+  nbf: number;
+  sub: number;
+  token_uuid: string;
+};
+
+export const getAccessToken = (): AccessToken | null => {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  const accessToken = getCookie("access_token");
+  if (!accessToken) {
+    return null;
+  }
+
+  const payload = <AccessToken | undefined>(
+    (<unknown>jose.decodeJwt(accessToken))
+  );
+  if (!payload) {
+    return null;
+  }
+
+  return payload;
+};
