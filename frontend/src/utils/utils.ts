55b101f1dc73c571558e747b993d0d76a8798a52
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-09-13 13:16:37 +0530

    Added a funciton to get the cookies from the client side

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -9,3 +9,15 @@ export const hasLinks = (
       }>
     | undefined,
 ): boolean => (data && data.pages[0].data.length !== 0 ? true : false);
+
+export const getCookie = (name: string): string | null => {
+  const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(`${name}=`))
+    ?.split("=")[1];
+  if (!cookieValue) {
+    return null;
+  }
+
+  return cookieValue;
+};
